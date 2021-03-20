91d55391525b8562867ba4d19dc680efffe4d613
akramnarejo <ak.narejo@gmail.com>
2021-03-20 23:55:15 +0500

    removed portfolio project

--- portfolio/projects/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from projects.models import Project
-
-class ProjectAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Project, ProjectAdmin)
